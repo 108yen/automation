8aca187f4b4e00c1c393e5409636e3a5795ff8c6
shirai02 <shirai-kazuki-yn@ynu.jp>
2019-09-24 04:09:16 +0900

    bitstreamファイルも同期

---
+++ project_1/project_1.sdk/out_prob/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../out_prob_bsp/microblaze_0/include/xparameters.h \
+ ../../out_prob_bsp/microblaze_0/include/xil_cache.h \
+ ../../out_prob_bsp/microblaze_0/include/mb_interface.h \
+ ../../out_prob_bsp/microblaze_0/include/xil_types.h \
+ ../../out_prob_bsp/microblaze_0/include/xil_assert.h \
+ ../../out_prob_bsp/microblaze_0/include/xil_exception.h \
+ ../../out_prob_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../out_prob_bsp/microblaze_0/include/xparameters.h:
+
+../../out_prob_bsp/microblaze_0/include/xil_cache.h:
+
+../../out_prob_bsp/microblaze_0/include/mb_interface.h:
+
+../../out_prob_bsp/microblaze_0/include/xil_types.h:
+
+../../out_prob_bsp/microblaze_0/include/xil_assert.h:
+
+../../out_prob_bsp/microblaze_0/include/xil_exception.h:
+
+../../out_prob_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
